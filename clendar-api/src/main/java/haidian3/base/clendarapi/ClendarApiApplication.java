504f62c3f92c38fcfbb5ec6e2ebc7089ccc1f0bd
zhangyx-v <1071050586@qq.com>
2020-10-29 10:12:06 +0800

    项目创建

---
+++ clendar-api/src/main/java/haidian3/base/clendarapi/ClendarApiApplication.java
@@ -0,0 +1,13 @@
+package haidian3.base.clendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClendarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClendarApiApplication.class, args);
+	}
+
+}
---
+++ clendar-api/src/test/java/haidian3/base/clendarapi/ClendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package haidian3.base.clendarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClendarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
